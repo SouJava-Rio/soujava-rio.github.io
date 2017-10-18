3e171c938ce69c05f4c8ca092983bea6c360805b
Fernando Arruda <arruda@Fernandos-MacBook-Air.local>
2017-10-18 00:43:32 -0200

    - Incluindo Setup de Integração com o Google Firebase(Realtime Database)

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+		//Aplicando evento no onclick dos itens do menu do topo
 		$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
           var e = a(this.hash);
@@ -13,4 +13,15 @@ $(document).ready(function() {
     window.sr = ScrollReveal();
     sr.reveal('.section-scrollable');
 
+		// Inicializando integração com o Google Firebase
+    var config = {
+      apiKey: "AIzaSyDd4_2LQis6drcZ_M940W_aHXlSkH5R4TA",
+      authDomain: "soujava-rio.firebaseapp.com",
+      databaseURL: "https://soujava-rio.firebaseio.com",
+      projectId: "soujava-rio",
+      storageBucket: "soujava-rio.appspot.com",
+      messagingSenderId: "143824838241"
+    };
+		
+    firebase.initializeApp(config);
 });
